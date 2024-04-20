0cabebb67c2d8ba6b4f174d5f6123ca8342220a4
c98181 <c98181@gmail.com>
2024-04-20 16:48:35 +0800

    n

--- Walker2d-v1/dataset/sample.py
+++ Walker2d-v1/dataset/sample.py
@@ -18,7 +18,7 @@ env = make_vec_env(
         lambda env, _: RolloutInfoWrapper(env)
     ],  # needed for computing rollouts later
     env_make_kwargs={"render_mode": "rgb_array"},
-    n_envs=1,
+    n_envs=100,
 )
 model = load_policy(
     "ppo-huggingface",
@@ -26,54 +26,44 @@ model = load_policy(
     env_name="seals/Walker2d-v1",
     venv=env,
 )
-import numpy as np
-from tqdm import tqdm
-obs = env.reset()
-done = False
-n_steps = 100000
-# num_episodes = 100
+rollout_info = rollout.generate_trajectories(model, env,rollout.make_sample_until(min_timesteps=10000000, min_episodes=None),rng=np.random.default_rng())
+# print(rollout_info[0])
+transitions = rollout.flatten_trajectories(rollout_info)
+print(transitions.obs.shape)
+# import numpy as np
+# from tqdm import tqdm
+# obs = env.reset()
+# done = False
+# n_steps = 10000000
+# # num_episodes = 100
 
-obs_list = []
-next_obs_list = []
-actions_list = []
-rewards_list = []
-dones_list = []
-info_list = []
-score_list=[]
-reward_sum = 0
-for i in tqdm(range(n_steps)):
-    action, _states = model.predict(obs, deterministic=True)
-    next_obs, reward, done, info = env.step(action)
-    reward_sum += reward
-    obs_list.append(obs)
-    next_obs_list.append(next_obs)
-    actions_list.append(action)
-    rewards_list.append(reward)
-    dones_list.append(done)
-    info_list.append(info)
-    obs = next_obs
-    if done:
-        obs = env.reset()
-        score_list.append(reward_sum)
-        reward_sum = 0
+# obs_list = []
+# next_obs_list = []
+# actions_list = []
+# rewards_list = []
+# dones_list = []
+# info_list = []
+# score_list=[]
+# reward_sum = 0
+# for i in tqdm(range(n_steps)):
+#     action, _states = model.predict(obs, deterministic=True)
+#     next_obs, reward, done, info = env.step(action)
+#     reward_sum += reward
+#     obs_list.append(obs)
+#     next_obs_list.append(next_obs)
+#     actions_list.append(action)
+#     rewards_list.append(reward)
+#     dones_list.append(done)
+#     info_list.append(info)
+#     obs = next_obs
+#     if done:
+#         obs = env.reset()
+#         score_list.append(reward_sum)
+#         reward_sum = 0
 
-obs_list = np.array(obs_list)
-actions_list = np.array(actions_list)
-rewards_list = np.array(rewards_list)
-dones_list = np.array(dones_list)
-np.save(os.path.join(os.path.dirname(__file__),"obs.npy"), obs_list)
-np.save(os.path.join(os.path.dirname(__file__),"actions.npy"), actions_list)
-np.save(os.path.join(os.path.dirname(__file__),"rewards.npy"), rewards_list)
-np.save(os.path.join(os.path.dirname(__file__),"dones.npy"), dones_list)
-np.save(os.path.join(os.path.dirname(__file__),"info.npy"), info_list)
-np.save(os.path.join(os.path.dirname(__file__),"next_obs.npy"), next_obs_list)
+np.save(os.path.join(os.path.dirname(__file__),"obs.npy"), transitions.obs)
+np.save(os.path.join(os.path.dirname(__file__),"actions.npy"), transitions.acts)
+# np.save(os.path.join(os.path.dirname(__file__),"dones.npy"), transitions.dones)
+# np.save(os.path.join(os.path.dirname(__file__),"info.npy"), transitions.infos)
+# np.save(os.path.join(os.path.dirname(__file__),"next_obs.npy"), transitions.next_obs)
 
-#　plot the score
-import matplotlib.pyplot as plt
-plt.plot(score_list)
-plt.xlabel("Episodes")
-
-plt.ylabel("Rewards")
-plt.title("Rewards of sample on walker2d")
-plt.savefig(os.path.join(os.path.dirname(__file__),"rewards.png"))
-plt.show()
