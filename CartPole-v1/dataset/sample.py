ff4571f977315e9fbe29d17c69b9114ac53cda04
c98181 <c98181@gmail.com>
2024-09-20 21:20:41 +0800

    D

--- CartPole-v1/dataset/sample.py
+++ CartPole-v1/dataset/sample.py
@@ -2,8 +2,8 @@ from stable_baselines3 import DQN, PPO
 from stable_baselines3.common.env_util import make_vec_env,make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
 
-env_id = "MountainCar-v0"
-model_path = "/mnt/nfs/work/c98181/rl-baselines3-zoo/rl-trained-agents/ppo/MountainCar-v0_1/MountainCar-v0.zip"  # 模型文件的路径
+env_id = "CartPole-v1"
+model_path = "/mnt/nfs/work/c98181/RL/CartPole-v1/dataset/CartPole-v1.zip"  # 模型文件的路径
 # env_id = "MsPacmanNoFrameskip-v4"
 # model_path = "/mnt/nfs/work/c98181/rl-baselines3-zoo/rl-trained-agents/dqn/MsPacmanNoFrameskip-v4_1/MsPacmanNoFrameskip-v4.zip" # 模型文件的路径
 
@@ -29,7 +29,8 @@ obs = env.reset()
 # print(initial_obs)
 done = False
 # sample 1M steps for CartPole and save to a numpy file
-n_steps = 28000
+# n_steps = 28000
+n_episodes = 1
 obs_list = []
 next_obs_list = []
 actions_list = []
@@ -39,22 +40,40 @@ info_list = []
 score_list=[]
 reward_sum = 0
 
-for i in tqdm(range(n_steps)):
-    action, _states = model.predict(obs, deterministic=True)
-    next_obs, rewards, dones, info = env.step(action)
+# for i in tqdm(range(n_steps)):
+#     action, _states = model.predict(obs, deterministic=True)
+#     next_obs, rewards, dones, info = env.step(action)
     
-    reward_sum += rewards[0]
-    obs_list.append(obs)
-    next_obs_list.append(next_obs)
-    actions_list.append(action)
-    rewards_list.append(rewards)
-    dones_list.append(dones)
-    info_list.append(info)
-    obs = next_obs
-    if dones[0]:
-        score_list.append(reward_sum)
-        reward_sum = 0
-        obs = env.reset()
+#     reward_sum += rewards[0]
+#     obs_list.append(obs)
+#     next_obs_list.append(next_obs)
+#     actions_list.append(action)
+#     rewards_list.append(rewards)
+#     dones_list.append(dones)
+#     info_list.append(info)
+#     obs = next_obs
+#     if dones[0]:
+#         score_list.append(reward_sum)
+#         reward_sum = 0
+#         obs = env.reset()
+
+for i in tqdm(range(n_episodes)):
+    obs = env.reset()
+    done = False
+    reward_sum = 0
+    while not done:
+        action, _states = model.predict(obs, deterministic=True)
+        next_obs, rewards, dones, info = env.step(action)
+        reward_sum += rewards[0]
+        obs_list.append(obs)
+        next_obs_list.append(next_obs)
+        actions_list.append(action)
+        rewards_list.append(rewards)
+        dones_list.append(dones)
+        info_list.append(info)
+        obs = next_obs
+        done = dones[0]
+    score_list.append(reward_sum)
     
 obs_list = np.array(obs_list)
 actions_list = np.array(actions_list)
@@ -63,13 +82,7 @@ dones_list = np.array(dones_list)
 # check shape 
 print(obs_list.shape, actions_list.shape, rewards_list.shape, dones_list.shape)
 print(obs_list[0], actions_list[0], rewards_list[0], dones_list[0])
-# /mnt/nfs/work/c98181/RL/MountainCar-v0/dataset
-np.save("/mnt/nfs/work/c98181/RL/MountainCar-v0/dataset/MountainCar-v0_28000_obs.npy", obs_list)
-np.save("/mnt/nfs/work/c98181/RL/MountainCar-v0/dataset/MountainCar-v0_28000_actions.npy", actions_list)
-np.save("/mnt/nfs/work/c98181/RL/MountainCar-v0/dataset/MountainCar-v0_28000_rewards.npy", rewards_list)
-np.save("/mnt/nfs/work/c98181/RL/MountainCar-v0/dataset/MountainCar-v0_28000_dones.npy", dones_list)
-np.save("/mnt/nfs/work/c98181/RL/MountainCar-v0/dataset/MountainCar-v0_28000_info.npy", info_list)
-np.save("/mnt/nfs/work/c98181/RL/MountainCar-v0/dataset/MountainCar-v0_28000_next_obs.npy", next_obs_list)
+
 
 #　plot the score
 import matplotlib.pyplot as plt
@@ -79,5 +92,13 @@ plt.xlabel("Episodes")
 plt.ylabel("Rewards")
 plt.title("Rewards of DQN on "+env_id)
 # dataset/MountainCar-v0_28000_rewards.png
-plt.savefig("/mnt/nfs/work/c98181/RL/MountainCar-v0/dataset/MountainCar-v0_28000_rewards.png")
+plt.savefig("/mnt/nfs/work/c98181/RL/CartPole-v1/dataset/rewards.png")
 plt.show()
+# save as /mnt/nfs/work/c98181/RL/CartPole-v1/dataset/CartPole-v1_28000_rewards.npy
+np.save("/mnt/nfs/work/c98181/RL/CartPole-v1/dataset/CartPole-v1_rewards.npy", score_list)
+np.save("/mnt/nfs/work/c98181/RL/CartPole-v1/dataset/CartPole-v1_obs.npy", obs_list)
+np.save("/mnt/nfs/work/c98181/RL/CartPole-v1/dataset/CartPole-v1_actions.npy", actions_list)
+np.save("/mnt/nfs/work/c98181/RL/CartPole-v1/dataset/CartPole-v1_rewards.npy", rewards_list)
+np.save("/mnt/nfs/work/c98181/RL/CartPole-v1/dataset/CartPole-v1_dones.npy", dones_list)
+np.save("/mnt/nfs/work/c98181/RL/CartPole-v1/dataset/CartPole-v1_info.npy", info_list)
+
